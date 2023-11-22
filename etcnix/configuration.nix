988e8287d80b06242405937d857fc2f523b04c02
pesekcuy <adi.nugroho@tutanota.com>
2023-11-23 02:03:00 +0700

    Change folder structure

--- etcnix/configuration.nix
+++ etcnix/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running `nixos-help`).
 
 { config, lib, pkgs, ... }:
 
@@ -16,26 +16,25 @@ in
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "jomblo-desktop"; # Define your hostname.
+  boot.initrd.luks.devices."luks-2860de7e-d394-4102-bda5-85caf600591a".device =  "/dev/disk/by-uuid/2860de7e-d394-4102-bda5-85caf600591a";
+  networking.hostName = "x1c6"; # Define your hostname.
+  # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Jakarta";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "id_ID.UTF-8";
     LC_IDENTIFICATION = "id_ID.UTF-8";
@@ -47,78 +46,25 @@ in
     LC_TELEPHONE = "id_ID.UTF-8";
     LC_TIME = "id_ID.UTF-8";
   };
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
-  # Enable the KDE Plasma Desktop Environment.
-  #services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmawayland";
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
   services.xserver = {
+    enable = true;
     layout = "us";
     xkbVariant = "";
+    desktopManager.plasma5.enable = true;
+    displayManager = {
+      lightdm.enable = true;
+      autoLogin.enable = true;
+      autoLogin.user = "pesekcuy";
+      defaultSession = "plasmawayland";
+    };
   };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.hplipWithPlugin ];
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pesekcuy = {
-    isNormalUser = true;
-    description = "pesekcuy";
-    extraGroups = [ "networkmanager" "wheel" "audio" "video" "input" "libvirtd" ];
-    packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
-    ];
-  };
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "pesekcuy";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    usbutils
-    pciutils
-    glib
-    cifs-utils
-    wineWowPackages.waylandFull
-    winetricks
-  ];
 
   environment.plasma5.excludePackages = with pkgs; [
     elisa
@@ -128,7 +74,57 @@ in
   ];
 
   programs.dconf.enable = true;
-  programs.partition-manager.enable = true;
+  programs.adb.enable = true;
+  programs.partition-manager.enable = true;  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+  };
+
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pesekcuy = {
+    description = "Adi Nugroho";
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" "input" "libvirtd" "adbusers" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      firefox
+      tree
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    usbutils
+    pciutils
+    glib
+    (python3.withPackages my-python-packages)
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   fonts.packages = with pkgs; [
     roboto
@@ -136,46 +132,39 @@ in
     noto-fonts-cjk
     noto-fonts-emoji
     liberation_ttf
-    source-han-sans
     (nerdfonts.override { fonts = [ "CodeNewRoman" ]; })
   ];
 
   fonts.fontconfig.defaultFonts = {
     serif = [ "Noto Serif" ];
     sansSerif = [ "Roboto" ];
-    monospace = [ "CodeNewRoman Nerd Font" ];
+    monospace = [ "CodeNewRoman Nerd Font Mono" ];
   };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    extraPackages = with pkgs; [ intel-media-driver ];
   };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-  programs.gamemode.enable = true;
-  programs.gamemode.settings.general.inhibit_screensaver = 0;
-
+  powerManagement.powertop.enable = true;
   systemd.services.mpd.environment = {
     # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
     XDG_RUNTIME_DIR = "/run/user/1000"; # User-id 1000 must matwch above user. MPD will look inside this directory for the PipeWire socket.
   };
-
-  # virtualization
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-  programs.virt-manager.enable = true;
 
   # List services that you want to enable:
 
   services.mpd = {
     enable = true;
     user = "pesekcuy";
-    musicDirectory = "/mnt/Data/Music";
+    musicDirectory = "/home/pesekcuy/Music";
     extraConfig = ''
       # must specify one or more outputs in order to play audio!
       # (e.g. ALSA, PulseAudio, PipeWire), see next sections
@@ -195,18 +184,90 @@ in
     startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  services.hardware.bolt.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-  # List services that you want to enable:
+	CPU_DRIVER_OPMODE_ON_AC = "active";
+        CPU_DRIVER_OPMODE_ON_BAT = "active";
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 100;
+
+	CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 1;
+
+        CPU_HWP_DYN_BOOST_ON_AC = 1;
+	CPU_HWP_DYN_BOOST_ON_BAT = 1;
+
+	PLATFORM_PROFILE_ON_AC = "balanced";
+	PLATFORM_PROFILE_ON_BAT = "balanced";
+
+	MEM_SLEEP_ON_AC = "deep";
+	MEM_SLEEP_ON_BAT = "deep";
+
+	START_CHARGE_THRESH_BAT0 = 75;
+	STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+  };
+
+  services.throttled = {
+    enable = true;
+    extraConfig =
+      ''
+	[GENERAL]
+	Enabled: True
+	Sysfs_Power_Path: /sys/class/power_supply/AC/online
+	Autoreload: True
+
+	[BATTERY]
+	Update_Rate_s: 30
+	PL1_Tdp_W: 15
+	PL1_Duration_s: 28
+	PL2_Tdp_W: 20
+	PL2_Duration_S: 0.002
+	Trip_Temp_C: 80
+	cTDP: 0
+	Disable_BDPROCHOT: False
+
+	[AC]
+	Update_Rate_s: 5
+	PL1_Tdp_W: 15
+	PL1_Duration_s: 28
+	PL2_Tdp_W: 20
+	PL2_Duration_S: 0.002
+	Trip_Temp_C: 80
+	cTDP: 0
+	Disable_BDPROCHOT: False
+
+	[UNDERVOLT.BATTERY]
+	CORE: -100
+	GPU: -100
+	CACHE: -100
+	UNCORE: -100
+	ANALOGIO: -100
+
+	[UNDERVOLT.AC]
+	CORE: -100
+	GPU: -100
+	CACHE: -100
+	UNCORE: -100
+	ANALOGIO: -100
+      '';
+  };
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  programs.virt-manager.enable = true;
 
   # samba
   services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
@@ -218,34 +279,23 @@ in
   ];
   services.samba = {
     enable = true;
-    openFirewall = true;
     securityType = "user";
     extraConfig = ''
       workgroup = WORKGROUP
       server string = smbnix
       netbios name = smbnix
-      security = user
+      security = user 
       #use sendfile = yes
       #max protocol = smb2
       # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.1. 192.168.0. 127.0.0.1 localhost
+      hosts allow = 192.168.0. 192.168.1. 192.168.122. 127.0.0.1 localhost
       hosts deny = 0.0.0.0/0
       guest account = nobody
       map to guest = bad user
     '';
     shares = {
-      data = {
-        path = "/mnt/Data";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "no";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "pesekcuy";
-        "force group" = "users";
-      };
-      videos = {
-        path = "/mnt/Videos/Videos";
+      public = {
+        path = "/home/pesekcuy/Public";
         browseable = "yes";
         "read only" = "no";
         "guest ok" = "yes";
@@ -254,8 +304,22 @@ in
         "force user" = "pesekcuy";
         "force group" = "users";
       };
+      private = {
+        path = "/home/pesekcuy/";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "pesekcuy";
+        "force group" = "users";
+      };
     };
   };
+
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -263,13 +327,20 @@ in
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
+  services.samba.openFirewall = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
+
